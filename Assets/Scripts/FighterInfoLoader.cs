5c8380db77b6a5358c6b766bcd128b7f76403617
digiholic <digikun@gmail.com>
2020-10-02 07:54:23 -0600

    Initial commit with project files

---
+++ Assets/Scripts/FighterInfoLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class FighterInfoLoader : MonoBehaviour
+{
+    public FighterInfo infoObject;
+    public TextAsset infoFile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (infoFile != null) infoObject = FighterInfo.fromJson(infoFile.text);
+        Debug.Log(infoObject.DisplayName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            Debug.Log(infoObject.toJson());
+            File.WriteAllText("Assets/test.json", infoObject.toJson());
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/FighterInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TUSSLE Fighter", menuName = "Modules/Fighter", order = 1)]
+public class FighterInfo : ScriptableObject
+{
+    /// <summary>
+    /// The Fighter's display name, which will be shown in the UI and otherwise used to identify the character
+    /// </summary>
+    public string DisplayName;
+
+
+
+    public string toJson()
+    {
+        return JsonUtility.ToJson(this);
+    }
+
+    public static FighterInfo fromJson(string jsonString)
+    {
+        FighterInfo info = ScriptableObject.CreateInstance<FighterInfo>();
+        JsonUtility.FromJsonOverwrite(jsonString, info);
+        return info;
+    }
+}
