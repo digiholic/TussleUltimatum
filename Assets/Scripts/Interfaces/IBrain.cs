3c14c5bea00bf633434789a9253a18758786fa11
digiholic <digikun@gmail.com>
2020-10-06 08:34:28 -0600

    Adds basic implementation for PlayerBrain and adds documentation to scripts

---
+++ Assets/Scripts/Interfaces/IBrain.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The Brain class is the method through which a fighter is controlled. All it does is "think" a set of commands every frame.
+/// How it comes to those commands is entirely up to the specific implementation. This class is used by PlayerBrains to read input,
+/// BotBrains to read game state and decide what to do, and NetworkBrains to get commands from the network connection
+/// </summary>
+public interface IBrain
+{
+    /// <summary>
+    /// The set of all thoughts that have been thought this frame. It is a set, so duplicates are ignored.
+    /// </summary>
+    /// <returns>A set of all thoughts the brain thinks this frame</returns>
+    HashSet<Thought> Think();
+}
---
+++ Assets/Scripts/MonoBehaviours/PlayerBrain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The PlayerBrain is an implementation of IBrain that reads input for a player and generates Thoughts from them
+/// </summary>
+public class PlayerBrain : MonoBehaviour, IBrain
+{
+    public HashSet<Thought> thoughtsThisFrame;
+
+    // Update is called once per frame
+    void Update()
+    {
+        thoughtsThisFrame = new HashSet<Thought>();
+
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+        
+        
+    }
+
+
+    public HashSet<Thought> Think()
+    {
+        return thoughtsThisFrame;
+    }
+}
