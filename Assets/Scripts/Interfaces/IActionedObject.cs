fedb233cf0d613f6a537127f8f11036bdd37906e
digiholic <digikun@gmail.com>
2020-10-06 21:11:44 -0600

    Adds several more interfaces for battle objects

---
+++ Assets/Scripts/Interfaces/IActionedObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An object that has an Action state machine.
+/// </summary>
+public interface IActionedObject
+{
+    /// <summary>
+    /// Gets the action this object is currently in
+    /// </summary>
+    /// <returns>The current action</returns>
+    TAction GetCurrentAction();
+
+    /// <summary>
+    /// Changes to the new Action
+    /// </summary>
+    /// <param name="newAction">the new action to change to</param>
+    void SetCurrentAction(TAction newAction);
+}
---
+++ Assets/Scripts/Interfaces/IThinkingObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An Interface for any object capable of generating Thoughts
+/// </summary>
+public interface IThinkingObject
+{
+    /// <summary>
+    /// Gets the current Brain of the object
+    /// </summary>
+    /// <returns>This object's Brain</returns>
+    IBrain GetBrain();
+
+    /// <summary>
+    /// Gets all of the thoughts that were generated since the last tick
+    /// </summary>
+    /// <returns>A Set of all Thoughts this Tick</returns>
+    HashSet<Thought> GetThoughts();
+}
