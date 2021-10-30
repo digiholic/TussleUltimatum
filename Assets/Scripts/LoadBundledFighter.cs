ba80a175863b841b861fa2570c9d4b17a6d13763
digiholic <digikun@gmail.com>
2021-10-30 09:35:24 -0600

    Adds in several libraries

---
+++ Assets/Scripts/LoadBundledFighter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadBundledFighter : MonoBehaviour
+{
+    public string bundleToLoad = "testfighter.ptf";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AssetBundle fighterBundle = AssetBundle.LoadFromFile($"Assets/AssetBundles/{bundleToLoad}");
+        string[] fighterScenes = fighterBundle.GetAllScenePaths();
+        SceneManager.LoadScene(fighterScenes[0], LoadSceneMode.Additive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TestBundleScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBundleScript : MonoBehaviour
+{
+    public string test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("A brand New Fighter! "+test);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
